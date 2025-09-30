509f14c30afa9b94d104ce99db2e5d2b5a335874
leonard-roepcke <leonard.roepcke.2@gmail.com>
2025-09-30 10:22:59 +0200

    simple linked list example

--- Main.java
+++ Main.java
@@ -1,6 +1,15 @@
-public class Main {
+public class Main {       // start gehört zur Klasse
+
     public static void main(String[] args) {
         System.out.println("Linked List Program");
+        Knoten start = new Knoten();
+        start.setNext(new Knoten());
+        start.getNext().setDaten(5);
+        start.getNext().setNext(new Knoten());
+        start.getNext().getNext().setDaten(10);
 
+        System.out.println("First node data: " + start.getDaten());
+        System.out.println("Second node data: " + start.getNext().getDaten());
+        System.out.println("Third node data: " + start.getNext().getNext().getDaten());
     }
 }
